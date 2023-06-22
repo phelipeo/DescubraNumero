d9f6f9e70b6fa77aa1791c9b13372d4074ae37c1
Phelipe <phelipelpos@gmail.com>
2023-06-22 16:58:26 -0300

    Aplitativo

---
+++ src/DescubraNumero.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class DescubraNumero
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int numero = rand.nextInt(101);
+        int tentativa = 0;
+
+        System.out.println("Descubra o número inteiro entre 0 e 100 em dez tentativas!");
+
+        for (int i=1 ; i<11 ; i++){
+            System.out.print("Tentativa: " + i + "! Por favor, digite um número: ");
+            tentativa = scanner.nextInt();
+            if (tentativa < 0 || tentativa > 100){
+                System.out.println("Digite somente números inteiros entre 0 e 100!");
+                i-=1;
+            }else if (tentativa == numero){
+                System.out.println("Parabéns, acertou!");
+                break;
+            }else if (tentativa > numero && i<10){
+                System.out.println("O número digitado é maior que o correto!");
+            }else if (tentativa < numero && i<10){
+                System.out.println("O número digitado é menor que o correto!");
+            }
+
+        }
+        if (numero != tentativa){
+            System.out.println("Você não conseguiu! O número era " + numero);
+        }
+    }
+}
+
